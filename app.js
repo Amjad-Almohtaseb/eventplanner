cba7fde8548fa8666948cb3330d5d4437eb8428c
Amjad-Almohtaseb <amjad.almohtaseb94@gmail.com>
2021-07-24 01:37:24 +0300

    finally

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const db = require("./db/models");
+
+app.use(express.json()); //middleware
+
+const eventRouter = require("./routers/eventRouter");
+
+app.use("/events", eventRouter);
+
+// db.sequelize.sync({alter : true});
+db.sequelize.sync();
+
+app.listen(8000);
---
+++ routers/eventRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const eventController = require("../controllers/eventController");
+
+router.get("/", eventController.eventsList);
+
+router.get("/fullybooked", eventController.fullyRes);
+
+router.get("/:eventId", eventController.eventDetail);
+
+router.post("/", eventController.eventCreate);
+
+router.delete("/:eventId", eventController.eventDelete);
+
+router.put("/:eventId", eventController.eventUpdate);
+
+module.exports = router;
